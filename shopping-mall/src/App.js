a824030977924d30dbe07cd7cc8c7239f8bf271c
jinseonghee <wlstjdgml0610@gmail.com>
2023-02-13 22:24:06 +0900

    feat: Implements of a detail page using routes, useParams

--- shopping-mall/src/App.js
+++ shopping-mall/src/App.js
@@ -5,10 +5,13 @@ import { useState } from 'react';
 import { Navbar,Container, Nav } from 'react-bootstrap';
 //import bg from './img/bg.png';
 import data from './data';
+import { Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom';
+import Detail from './routes/Detail';
 
 function App() {
 
   let [shoes] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -16,29 +19,62 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link onClick={() => {
+              {navigate('/')}
+            }}>Home</Nav.Link>
+            {/*   {navigate(-1) 이렇게 넣어주면 뒤로 가기 처럼 한단계 뒤로 간다} */}
+            <Nav.Link onClick={() => {
+              {navigate('/detail')}
+            }}>Cart</Nav.Link>
+            {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
           </Nav>
         </Container>
       </Navbar>
-
-      <div className = "main-bg" 
-      //style={{backgroundImage: 'url('+ bg +')'}}
-      >
-      </div>
-      <div className="Container">
-        <div className="row">
-          {/* <Card shoes={shoes[0]} i={1}/>
-          <Card shoes={shoes[1]} i={2}/>
-          <Card shoes={shoes[2]} i={3}/> */}
-          {
+      <Routes>
+        <Route path="/" element={
+           <>
+          <div className = "main-bg" 
+             //style={{backgroundImage: 'url('+ bg +')'}}
+            >
+          </div>
+           <div className="Container">
+           <div className="row">
+           {
             shoes.map((a, i) => {
             return(
               <Card shoes={shoes[i]} i={i+1}/>
               )
             })
           }
+           </div>
+        </div>
+      </>
+        }>
+        </Route>
+        <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
+        <Route path="*" element={<div>없는페이지임</div>}/>
+        <Route path="/about" element={ <About/> } >  
+        <Route path="member" element={ <div>멤버들</div> } />
+        <Route path="location" element={ <div>회사위치</div> } />
+      </Route>
+      </Routes>
+
+      {/* <div className = "main-bg" 
+      //style={{backgroundImage: 'url('+ bg +')'}}
+      >
+      </div> */}
+      {/* <div className="Container">
+        <div className="row"> */}
+          {/* <Card shoes={shoes[0]} i={1}/>
+          <Card shoes={shoes[1]} i={2}/>
+          <Card shoes={shoes[2]} i={3}/> */}
+          {/* {
+            shoes.map((a, i) => {
+            return(
+              <Card shoes={shoes[i]} i={i+1}/>
+              )
+            })
+          } */}
           {/* <div className="col-md-4">
             <img src = "https://codingapple1.github.io/shop/shoes1.jpg" width="80%"></img>
             <img src={process.env.PUBLIC_URL + '/logo192.png'} />  public 폴더에 img를 넣을 떄 사용하는 방식
@@ -55,10 +91,18 @@ function App() {
             <h4>상품명</h4>
             <p>상품설명</p>
           </div> */}
-        </div>
-      </div>
-    </div>
+         </div>
+      // </div>
+    // </div>
   );
+}
+function About(){
+  return (
+    <div>
+      <h4>about페이지임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 function Card(props) {
