19ee872bc475f67e85c54888c079fc00ddf2eab8
jinseonghee <wlstjdgml0610@gmail.com>
2023-02-13 23:32:45 +0900

    feat: used a useEffect hook

--- shopping-mall/src/routes/Detail.js
+++ shopping-mall/src/routes/Detail.js
@@ -1,19 +1,49 @@
 import { useParams } from "react-router-dom";
+import {useEffect, useState } from "react";
+//import styled from 'styled-components'
+
+// let Box = styled.div`
+//   padding : 20px;
+//   color : grey
+// `;
+
+// let YellowBtn = styled.button` 
+//   background : ${ props => props.bg };
+//   color : ${ props => props.bg == 'blue' ? 'white' : 'black' };
+//   padding : 10px; 
+// `; 
 
 function Detail(props) {
 
     let {id} = useParams();
+    let 찾은상품 = props.shoes.find(x => x.id == id);
+    let [alert, setAlert] = useState(true);
+
+    useEffect(()=>{
+        setTimeout(()=>{ setAlert(false) }, 2000)
+      }, [])
+    
 
     return(
+         
         <div className="container">
+        {
+                 alert == true
+                 ? <div className="alert alert-warning">
+                     2초이내 구매시 할인
+                   </div>
+                 : null
+        }
         <div className="row">
             <div className="col-md-6">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
             </div>
             <div className="col-md-6">
-            <h4 className="pt-5">{props.shoes[id].title}</h4>
-            <p>{props.shoes[id].content}</p>
-            <p>{props.shoes[id].price}원</p>
+            {/* <h4 className="pt-5">{props.shoes[id].title}</h4> */}
+            <h4 className="pt-5">{찾은상품.title}</h4>
+            {/* <p>{props.shoes[id].content}</p> */}
+            <p>{찾은상품.content}</p>
+            <p>{찾은상품.price}원</p>
             <button className="btn btn-danger">주문하기</button> 
             </div>
         </div>
